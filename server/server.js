928a20d7ffff45661de3381cb52f868bc0e7c6da
Sheikh Sohan <104677321+sheikhsohan@users.noreply.github.com>
2023-03-12 15:42:49 +0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import { Configuration, OpenAIApi } from 'openai';
 
-dotenv.config();
+dotenv.config()
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -12,40 +12,38 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const app = express();
+const app = express()
+app.use(cors())
+app.use(express.json())
 
-// middleware
-app.use(cors());
-app.use(express.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// routes
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'Hello from SOHAN - OpenAI World',
-  });
-});
+    message: 'Hello from SohanAi'
+  })
+})
 
 app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
+
     const response = await openai.createCompletion({
-      model: 'text-davinci-003',
+      model: "text-davinci-003",
       prompt: `${prompt}`,
-      temperature: 0,
-      maxTokens: 3000,
-      top_p: 1,
-      frequencyPenalty: 0.5,
-      presencePenalty: 0,
+      temperature: 0, // Higher values means the model will take more risks.
+      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
     });
 
     res.status(200).send({
       bot: response.data.choices[0].text
     });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ error });
-  }
-});
 
-app.listen(5000, () => console.log('Serve is running on port http://localhost:5000'));
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong');
+  }
+})
+
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
